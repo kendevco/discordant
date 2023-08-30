25004ce7f7b3dba4c10f4faaa6053454d5fa9bac
Kenneth Courtney <kenneth.courtney@gmail.com>
2023-08-30 14:49:25 -0400

    Initial commit

---
+++ app/(auth)/(routes)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return ( 
+        <div>
+            This is a protected route.
+        </div>
+     );
+}
+ 
+export default Home;
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div >
+      <p className='text-3xl font-bold text-indigo-500'>Hello Discord Clone!</p>
+      <Button>Click me</Button>
+    </div>
+     
+  )
+}
