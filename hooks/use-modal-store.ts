b675d216b8ee2eda817531137474f432cb054895
Kenneth Courtney <kenneth.courtney@gmail.com>
2023-09-01 15:53:18 -0400

    With Server Settings modal.

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,7 +1,7 @@
 import { ServerWithMembersWithProfiles } from '@/types';
 import { create } from 'zustand';
 
-export type ModalType = "createServer" | "invite"; // | "editServer" | "createChannel"
+export type ModalType = "createServer" | "invite" | "editServer"; // | "createChannel"
 
 interface ModalData {
     server?: string | ServerWithMembersWithProfiles;
